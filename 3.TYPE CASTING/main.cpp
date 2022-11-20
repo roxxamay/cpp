087c7339642a32641a46a549d95f04a13b28fdc2
ADITYA KUMAR <itsmeadityakumararya@gmail.com>
2022-11-20 17:28:36 +0530

    second commit on date 20th nov 2022

---
+++ 3.TYPE CASTING/main.cpp
@@ -0,0 +1,25 @@
+/*
+*  this simple program will help to understand the concept of type casting
+*
+*  author:  @roxxamay
+*/
+#include"iostream"
+
+using namespace std;
+
+int main()
+{
+    int a='a';
+    cout  <<  "a="  <<  a  <<  endl;
+
+    //see we input a='a' but the result is giving 97 that is because the ascii value of a is 97
+
+    // let see another example
+
+    char ch=98;
+
+    cout  <<  "ch="  <<  ch  <<  endl;
+
+    //see we input ch=98 but the result is showing b that is because the ascii value of b is 98
+
+}
